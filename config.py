b65d5706e3852765312ca75f775b78a02d4043d8
kajla4053-hash <kajla4053@gmail.com>
2026-03-14 20:47:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ import os
 
 API_ID = os.environ.get("API_ID", "22480303") #Replace With your api id
 API_HASH = os.environ.get("API_HASH", "99c931b6c1ae6f8c3c3e87da173fa424") #Replace With your api hash
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8466521986:AAG99521xxQtqR7hJsoIthUJwgb4XXrknE0") #Replace With your bot token
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8345019432:AAHw1S7vzgqju31ytGL6MMUGt6cBZuPJjHY") #Replace With your bot token
