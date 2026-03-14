35b3990d082d1f442d3f0bd163d7030fa410cb2a
premiumytmk234-coder <premiumytmk234@gmail.com>
2026-03-14 23:57:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -352,7 +352,7 @@ async def upload(bot: Client, m: Message):
                 keys_string = " ".join([f"--key {key}" for key in keys])
               
             elif "d1d34p8vz63oiq" in url or "sec1.pw.live" in url:
-             url = f"https://anonymouspwplayerrr-3dba7e3fb6a8.herokuapp.com/pw?url={url}&token={pw_token}"
+             url = f"https://anonymouspwplayerrr-3dba7e3fb6a8.herokuapp.com/pw?url={url}&token={raw_text4}"
               
             elif "acecwply" in url:
                 cmd = f'yt-dlp -o "{name}.%(ext)s" -f "bestvideo[height<={raw_text2}]+bestaudio" --hls-prefer-ffmpeg --no-keep-video --remux-video mkv --no-warning "{url}"'
